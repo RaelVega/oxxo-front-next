c9f1fd569ad9bcb487133c81f03845c5e949b085
RaelVega <raelvega33@gmail.com>
2024-10-17 16:00:09 -0600

    sidebar with home

---
+++ app/dashboard/_components/_sidebar/NavItem.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface NavItemProps {
+    icon: ReactNode
+    path: string
+}
+
+const NavItem = ({ icon, path }: NavItemProps) => {
+    return (
+        <Link href={path}>
+        {icon}
+        </Link>
+    )       
+}
+
+
+export default NavItem;
+
